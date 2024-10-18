e52486f254b11adde550f95cc3ea5c5c41657b85
emn <ezekiel.nye@forfusion.com>
2024-10-18 11:14:44 +0100

    Add first document!
    Thanks Dan

--- markdoc.config.mjs
+++ markdoc.config.mjs
@@ -1,12 +1,12 @@
-import { defineMarkdocConfig, component } from '@astrojs/markdoc/config';
-import starlightMarkdoc from '@astrojs/starlight-markdoc';
+import { defineMarkdocConfig, component, nodes } from "@astrojs/markdoc/config"
+import starlightMarkdoc from "@astrojs/starlight-markdoc"
 
 // https://docs.astro.build/en/guides/integrations-guide/markdoc/
 export default defineMarkdocConfig({
-	extends: [starlightMarkdoc()],
-	tags: {
-		FrontPage: {
-		  render: component('./src/components/FrontPage.astro'),
-		},
-	  },
-});
+  extends: [starlightMarkdoc()],
+  tags: {
+    FrontPage: {
+      render: component("./src/components/FrontPage.astro"),
+    },
+  },
+})
